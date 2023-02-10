8bc5749dd8122fd6446ce1bb7b0d40202db4803b
Przemysław Warchoł <warcholprzemo@gmail.com>
2023-02-10 23:45:41 +0100

    Add requirements.txt, Dockerfile and docker-compose.yml

--- tasks/database.py
+++ tasks/database.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_DETAILS = "mongodb://mongoservice:27017"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
