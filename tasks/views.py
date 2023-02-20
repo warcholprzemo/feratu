32735f0695a3220c8b5d9b0853272dd4c88ed373
Przemysław Warchoł <warcholprzemo@gmail.com>
2023-02-20 23:37:58 +0100

    Add CORSMiddleware (for frontend in Vue.js)

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, Response, status
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import add_task, delete_task, retrieve_tasks, update_task
 from .models import FullTask, Task
 
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
